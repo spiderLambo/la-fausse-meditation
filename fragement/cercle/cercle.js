eecc16b185c6355b6a3ecaaf0f5163fc89f9367b
Spider Lambo <spiderLambo.dev@outlook.com>
2024-01-10 20:24:24 +0100

    fin du cercle

--- fragement/cercle/cercle.js
+++ fragement/cercle/cercle.js
@@ -3,7 +3,7 @@ const boutton = document.querySelector("button"); // Element servant à commence
 const cercle = document.querySelector(".cercle"); // Element servant de cercle
 const respiration = document.querySelector(".respiration"); // Element servant d'indicateur sur la respiration
 
-let tempsEnSecondes = 2; // definition du temps en secondes
+let tempsEnSecondes = 90; // definition du temps en secondes
 let minute; // Définition du nombre de minutes
 let secondes; // Définition du nombre de secondes
 let tailleCercle = 30; // Définition de la taille du cercle
@@ -52,36 +52,30 @@ boutton.addEventListener("click", () => {
     tempsEnSecondes = tempsEnSecondes <= 0 ? 0 : tempsEnSecondes - 1; // Si le temps en secondes est inferieur ou egal à 0 alors, on ne change pas sa valeur, sinon, on retire 1
 
     // Animation faite en fonction du nombre de secondes
+    // Moment ou la réduction de taille s'éffectue
     if (
-      tempsEnSecondes >= 70 &&
-      (tempsEnSecondes % 4 === 1 || tempsEnSecondes % 4 === 2)
+      (tempsEnSecondes >= 70 &&
+        (tempsEnSecondes % 4 === 1 || tempsEnSecondes % 4 === 2)) ||
+      (70 > tempsEnSecondes && tempsEnSecondes >= 66) ||
+      (60 > tempsEnSecondes && tempsEnSecondes >= 50) ||
+      30 > tempsEnSecondes
     ) {
       reduireTaille();
     }
+    // Moment ou l'augmentation de taille s'éffectue
     if (
-      tempsEnSecondes >= 70 &&
-      (tempsEnSecondes % 4 === 3 || tempsEnSecondes % 4 === 0)
+      (tempsEnSecondes >= 70 &&
+        (tempsEnSecondes % 4 === 3 || tempsEnSecondes % 4 === 0)) ||
+      (66 > tempsEnSecondes && tempsEnSecondes >= 60) ||
+      (50 > tempsEnSecondes && tempsEnSecondes >= 30)
     ) {
       augmenterTaille();
     }
-    if (70 > tempsEnSecondes && tempsEnSecondes >= 66) {
-      reduireTaille();
-    }
-    if (66 > tempsEnSecondes && tempsEnSecondes >= 60) {
-      augmenterTaille();
-    }
-    if (60 > tempsEnSecondes && tempsEnSecondes >= 50) {
-      reduireTaille();
-    }
-    if (50 > tempsEnSecondes && tempsEnSecondes >= 30) {
-      augmenterTaille();
-    }
-    if (30 > tempsEnSecondes) {
-      reduireTaille();
-    }
+    // Début de la rotation du cercle
     if (20 > tempsEnSecondes) {
       cercle.style.animation = "rotation 1s linear infinite";
     }
+    // Fin de toutes les animations
     if (0 === tempsEnSecondes) {
       arret();
     }
