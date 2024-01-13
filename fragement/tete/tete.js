caa25763852751d91384fda40194bd313f2af6d7
Spider Lambo <spiderLambo.dev@outlook.com>
2024-01-13 14:42:31 +0100

    tete update

---
+++ fragement/tete/tete.js
@@ -0,0 +1,4 @@
+document.querySelector("button").addEventListener("click", () => {
+  document.querySelector("#tete ul").style.animation = "remonter 40s linear";
+});
+// Quand on clique sur le boutton, l'animation se déclanche
