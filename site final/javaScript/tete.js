4fa71295d1c45125d96a254b8b95fa830d28b447
Spider Lambo <spiderLambo.dev@outlook.com>
2024-01-14 17:38:03 +0100

    simulation update

--- site final/javaScript/tete.js
+++ site final/javaScript/tete.js
@@ -1,4 +1,4 @@
 document.querySelector(".tetebtn").addEventListener("click", () => {
-  document.querySelector("#tete ul").style.animation = "remonter 40s linear";
+  document.querySelector("#tete ul").style.animation = "remonter 90s linear";
 });
 // Quand on clique sur le boutton, l'animation se déclanche
